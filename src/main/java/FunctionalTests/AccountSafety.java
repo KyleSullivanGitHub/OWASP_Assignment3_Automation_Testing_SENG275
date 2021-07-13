16dc5d9aa81519d30cc034f9a291b0b22c0f9e39
kylesullivan <kylesullivan@uvic.ca>
2021-07-13 12:56:48 -0700

    V0.3.1.5:  (Kyle Sullivan) oops, small fixed mistake

--- src/main/java/FunctionalTests/AccountSafety.java
+++ src/main/java/FunctionalTests/AccountSafety.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.ITest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
@@ -20,7 +21,7 @@ import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.spi.ToolProvider;
 
-public class AccountSafety
+public class AccountSafety implements ITest
 {
     private ThreadLocal<String> testName = new ThreadLocal<>(); //Thread for renaming tests in console
     String website = "https://juice-shop.herokuapp.com"; //default website URL
