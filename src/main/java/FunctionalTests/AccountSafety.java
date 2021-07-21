8abce80bc9025dfa6e4ede08a35e5b213eb62849
Nicole Makarowski <nmakarowski@uvic.ca>
2021-07-20 20:30:38 -0700

    Changing BeforeSuite to BeforeClass

--- src/main/java/FunctionalTests/AccountSafety.java
+++ src/main/java/FunctionalTests/AccountSafety.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.ITest;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
